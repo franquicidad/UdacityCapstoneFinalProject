3d1ad7ad2fb6827f7c6f37e3623578f3b7a841cb
mac <franquicidad@gmail.com>
2018-09-19 22:17:26 -0500

    Added collapsing toolbar! and added a translucent toolbar completed succesfully

--- app/src/main/java/com/example/mac/udacitycapstonefinalproject/CarDetailView.java
+++ app/src/main/java/com/example/mac/udacitycapstonefinalproject/CarDetailView.java
@@ -1,10 +1,13 @@
 package com.example.mac.udacitycapstonefinalproject;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -61,10 +64,14 @@ public class CarDetailView extends AppCompatActivity {
         setContentView(R.layout.detail_collapsing_toolbar);
         ButterKnife.bind(this);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         automoviles = getIntent().getExtras().getParcelable("ListaVehiculos");
         toolbar.setTitle(automoviles.getMarca());
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+
         detalleModelo.setText(automoviles.getModelo() + "");
         if (!automoviles.isNuevo()) {
             lykilometraje.setVisibility(View.VISIBLE);
@@ -87,10 +94,6 @@ public class CarDetailView extends AppCompatActivity {
                 setAppbarOffset(heigthPx);
             }
         });
-
-
-
-
     }
 
     private void setAppbarOffset(int heigthPx) {
